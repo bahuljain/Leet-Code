570693e0de915abab22417cdcad6f2b65f3428fe
bahuljain <bkj2111@columbia.edu>
2015-11-07 22:13:14 -0500

    11/7/2015

--- PascalTriangle.java
+++ PascalTriangle.java
@@ -1,4 +1,4 @@
-public class PascalTraingle {
+public class PascalTrainglee {
     public List<List<Integer>> generate(int numRows) {
         List<List<Integer>> pas = new ArrayList<>();
 
@@ -29,4 +29,32 @@ public class PascalTraingle {
 
         return pas;
     }
+
+    public List<List<Integer>> generate(int numRows) {
+
+        ArrayList<List<Integer>> rt = new ArrayList<List<Integer>>();
+
+        Integer[] prev = null;
+
+        for(int i = 1 ; i <= numRows; i++){
+
+            Integer[] row = new Integer[i];
+
+            row[0] = 1;
+            row[i - 1] = 1;
+
+            for(int j = 1; j < i - 1 ; j++){
+                //row.add(j, prev.get(j) + prev.get(j -1));
+                row[j] = prev[j] + prev[j - 1];
+            }
+
+            rt.add(new ArrayList<Integer>(Arrays.asList(row)));
+            prev = row;
+        }
+
+
+        return rt;
+
+    }
+
 }
