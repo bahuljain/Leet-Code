a155b33af7f5299820d9bb0caaaf17cd73e0f897
bahuljain <bkj2111@columbia.edu>
2015-11-09 15:54:39 -0500

    adding contraints

--- UniquePaths.java
+++ UniquePaths.java
@@ -1,5 +1,7 @@
 public class UniquePaths {
     public int uniquePaths(int m, int n) {
+        if (m == 0 || n == 0) return 0;
+
         int[][] paths = new int[m+1][n+1];
         for(int i = 1; i <= m; i++) {
             for(int j = 1; j <= n; j++) {
