cfe2b90325253f91dd346bdb21c79027a72fb89c
bahuljain <bkj2111@columbia.edu>
2015-11-09 15:52:56 -0500

    11/9/2015

---
+++ UniquePaths.java
@@ -0,0 +1,15 @@
+public class UniquePaths {
+    public int uniquePaths(int m, int n) {
+        int[][] paths = new int[m+1][n+1];
+        for(int i = 1; i <= m; i++) {
+            for(int j = 1; j <= n; j++) {
+                if(i == 1 || j == 1) paths[i][j] = 1;
+                else {
+                    paths[i][j] = paths[i-1][j] + paths[i][j-1];
+                }
+            }
+        }
+        return paths[m][n];
+
+    }
+}
