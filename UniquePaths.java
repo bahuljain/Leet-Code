e8ee183e138210fa931ee8db92341d54d92f844d
bahuljain <bkj2111@columbia.edu>
2015-11-09 15:55:41 -0500

    cleaning code

--- UniquePaths.java
+++ UniquePaths.java
@@ -6,9 +6,7 @@ public class UniquePaths {
         for(int i = 1; i <= m; i++) {
             for(int j = 1; j <= n; j++) {
                 if(i == 1 || j == 1) paths[i][j] = 1;
-                else {
-                    paths[i][j] = paths[i-1][j] + paths[i][j-1];
-                }
+                else paths[i][j] = paths[i-1][j] + paths[i][j-1;
             }
         }
         return paths[m][n];
