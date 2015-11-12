65638e337a1a6f506daf49b70c000ddf8d08a06e
bahuljain <bkj2111@columbia.edu>
2015-11-12 12:15:54 -0500

    adding correct answer and another with streams

--- MajorityElement.java
+++ MajorityElement.java
@@ -17,4 +17,35 @@ public class MajorityElement {
         return -1;
 
     }
+
+    // that's the correct answer, mine is a dumb shit answer.
+    public int majorityElement(int[] num) {
+        int m = num[0];
+        int c = 1;
+
+        for(int i = 1; i < num.length; i++){
+            if (num[i] == m) {
+                c++;
+            } else if (c > 1) {
+                c--;
+            } else { // c == 1 && num[i] != m
+                m = num[i];
+            }
+        }
+
+        return m;
+    }
+
+    // using streams... another interesting solution
+    public int majorityElement(int[] nums) {
+        Map<Integer, List<Integer>> m = IntStream.of(nums).boxed()
+                .collect(Collectors.groupingBy((i)->i));
+
+        for (Map.Entry<Integer,List<Integer>> entry : m.entrySet()) {
+            if(entry.getValue().size() > nums.length/2)
+                return entry.getKey();
+        }
+
+        return -1;
+    }
 }
