982b26dc4c820ecd61ff368d1cb920bbb79f5ba0
bahuljain <bkj2111@columbia.edu>
2015-11-12 12:27:44 -0500

    11/12/2015

---
+++ MaxDepth.java
@@ -0,0 +1,20 @@
+public class MaxDepth {
+
+    public int maxDepth(TreeNode root) {
+        if(root == null) {
+            return 0;
+        } else {
+            int left = maxDepth(root.left);
+            int right = maxDepth(root.right);
+            if(left > right)
+                return left + 1;
+            else
+                return right + 1;
+        }
+    }
+
+    public int maxDepth(TreeNode root) {
+         if(root == null) return 0;
+         return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+     }
+}
