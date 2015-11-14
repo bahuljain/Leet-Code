9405b5a8e61498dbbc25e78a60d4d51a91952be8
bahuljain <bkj2111@columbia.edu>
2015-11-14 15:04:29 -0500

    adding correct answer

--- InvertBinaryTree.java
+++ InvertBinaryTree.java
@@ -1,5 +1,18 @@
 public class InvertBinaryTree {
     public TreeNode invertTree(TreeNode root) {
+        if(root == null) return null;
+
+        TreeNode newLeft  = invertTree(root.right);
+        TreeNode newRight = invertTree(root.left);
+
+        root.left  = newLeft;
+        root.right = newRight;
+
+        return root;
+    }
+
+  //this is some shitty method
+    public TreeNode invertTree(TreeNode root) {
         if(root == null)
             return null;
 
@@ -31,4 +44,5 @@ public class InvertBinaryTree {
 
         return root;
     }
+
 }
