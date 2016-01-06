331f75928099d565bec935ca3eae1d15eb469c33
bahuljain <bkj2111@columbia.edu>
2016-01-06 11:17:27 -0500

    correct answer

--- SingleNumberIII.java
+++ SingleNumberIII.java
@@ -1,19 +1,24 @@
-import java.util.Map.Entry;
+http://www.codinghelmet.com/?path=exercises/two-numbers-appearing-once-in-array
 
 public class Solution {
     public int[] singleNumber(int[] nums) {
-        Map<Integer,Long> m = Arrays
-				.stream(nums)
-				.boxed()
-				.collect(Collectors.groupingBy(s-> s, Collectors.counting()));
-
-		ArrayList<Integer> result = new ArrayList();
-
-		for (Entry<Integer,Long> entry : m.entrySet()) {
-			if(entry.getValue()==1)
-				result.add(entry.getKey());
-		}
-
-		return result.stream().mapToInt(i->i).toArray();
+        // go through the array and XOR every element, for example, result = 6 (3^5)
+        int result = nums[0];
+        for(int i = 1; i < nums.length; i++){
+            result ^= nums[i];
+        }
+        // notice that 6^5 = 3, 6^3 = 5
+        // now how to find 3 and 5 from 6
+        int[] r = new int[2];
+        // find the lowest bit of the result
+        // let's say 6 (0110), -6 = 1010  0110 & 1010 = 0010
+        int lowbit = result & -result;
+        // since this bit from the result is 1, we can be sure that
+        // a & lowbit and b & lowbit have different result
+        for(int n : nums){
+            if((n & lowbit) == 0) r[0] ^= n;
+            else r[1] ^=n;
+        }
+        return r;
     }
 }
